ee75a75533cd2dc2901106bfccdcc62765322e89
l33tp3ak <m431f2@gmail.com>
2026-01-17 15:23:44 -0500

    Created the first modular prop

---
+++ frontend/src/components/MainContent.tsx
@@ -0,0 +1,31 @@
+
+import {useState, React} from 'react';
+import '../assets/CSS/styleindex.css';
+
+
+/*
+	* This allows us to put children elements inside of our prop, making it more modular and speeding up development time.
+	* By writing the children as "children?", we make the children optional.
+	* If we write the children as "children", they are mandatory.
+*/
+type Props = {
+				children?: React.ReactNode;
+};
+
+//
+export const MainContent = ({children}: Props) => {
+				const [count, setCount] = useState(0);
+
+				return (
+								<>
+												<div className="center">
+																<div id="contenu">
+																				<div className="left contenu">
+																								{children}
+																				</div>
+																</div>
+												</div>
+								</>
+				)
+};
+
