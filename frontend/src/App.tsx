7dc7c2b8057d823f06416512f6d09fe00408c125
l33tp3ak <m431f2@gmail.com>
2026-01-17 17:21:17 -0500

    Created the first modular prop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './assets/CSS/styleindex.css';
 import {MainContent} from "./components/MainContent.tsx";
+import {Menu} from "./components/Menu.tsx";
 
 function App() {
 				const [count, setCount] = useState(0);
 
 				return (
 								<>
+												<nav className="navBar">
+																<Menu></Menu>
+												</nav>
 												<div>
 																<MainContent>
 																				<h1>Test</h1>
